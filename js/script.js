b690961485b1a33552171806f50e015311e0813f
Andrew Tereshin <andreyarbul@gmail.com>
2019-02-27 18:14:35 +0300

    #5.1
    Неожиданно заработала плавная прокрутка

--- js/script.js
+++ js/script.js
@@ -67,4 +67,9 @@ $(function(){
 
 	$('.popup-container__callback__form__phone').mask('+7 (999) 999-99-99');
 
+	$("body").on('click', '[href*="#"]', function(e){
+	  var fixed_offset = 0;
+	  $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
+	  e.preventDefault();
+	});
 });
