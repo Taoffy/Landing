132897d4483da847edee8cd8fd30985893d97825
Andrew Tereshin <andreyarbul@gmail.com>
2019-02-27 18:08:18 +0300

    #5
    Сделан попап, добавлены все функции, кроме плавной прокрутки

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
 $(function(){
+
+	function disableScroll(){
+		$('html, body').on('touchmove', function(){
+			preventDefault();
+		});
+		$('html, body').css('overflow', 'hidden');
+	}
+	function enableScroll(){
+		$('html, body').off('touchmove');
+		$('html, body').css('overflow-y', 'auto');
+	}
+
+
 	$('.examples_slick').slick({
 	  infinite: true,
 	  slidesToShow: 3,
@@ -21,12 +34,37 @@ $(function(){
 	      }
 	    }]
 	});
-});
 
-$(document).ready(function(){
+	$('.head__call-order, .footer_call-order').click(function() {
+		$('.popup-container, .popup-container__callback').fadeIn(200);
+		disableScroll();
+	});
+
+	$('.popup-container').click(function(event){
+		if (event.target == this) {
+			$('.popup-container, .popup-container__callback').fadeOut(50);
+			enableScroll();
+		};
+	});
+
+	$('.about-me__learn-more, .what-i-do_price, .examples_order-button_order-project').click(function(){
+		$('.popup-container, .popup-container__question').fadeIn(200);
+		disableScroll();
+	});
+
+	$('.popup-container').click(function(event){
+		if (event.target == this) {
+			$('.popup-container, .popup-container__question').fadeOut(50);
+			enableScroll();
+		};
+	});
+
 	$('.menu-mob').hide();
 	$('#nav-icon').click(function(){
 		$(this).toggleClass('open');
 		$('.menu-mob').fadeToggle('slow', 'linear');
 	});
+
+	$('.popup-container__callback__form__phone').mask('+7 (999) 999-99-99');
+
 });
